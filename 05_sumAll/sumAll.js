9d63174fea2160a2ae7eb76a7b9a6ff819b5dd60
Yadola <Ya-dola@users.noreply.github.com>
2023-03-16 20:54:21 +0530

    Completed SumAll Tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = (num1, num2) => {
+    // Check if both arguments are numbers
+    if (typeof num1 !== 'number' || typeof num2 !== 'number')
+        return 'ERROR';
 
+
+    // Check if either argument is negative
+    if (num1 < 0 || num2 < 0)
+        return 'ERROR';
+
+
+    // Determine the minimum and maximum values
+    const min = Math.min(num1, num2);
+    const max = Math.max(num1, num2);
+
+    // Calculate the sum of all integers in the range
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
